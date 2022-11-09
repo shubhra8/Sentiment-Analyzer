aaa4236a29c4465fe2bf8ea8a0739eb794be0f07
Shubhra Salunke <shubrasalunkecse@gmail.com>
2022-11-09 10:35:47 -0600

    Update index.js

--- src/server/index.js
+++ src/server/index.js
@@ -26,7 +26,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json());
 
-app.use(express.static('/'))
+app.use(express.static('dist'))
 
 console.log(__dirname)
 
@@ -51,7 +51,7 @@ app.get('/', function (req, res) {
    // res.sendFile("dist/index.html")
    // res.sendFile("C:/evaluate-news-nlp/dist/index.html");
    // res.sendFile('index.html', { root: 'C:/evaluate-news-nlp/dist' })
-    res.sendFile(path.join(__dirname, '/dist/index.html'));
+    res.sendFile(path.join(__dirname, '/index.html'));
 })
 
 // designates what port the app will listen to for incoming requests
