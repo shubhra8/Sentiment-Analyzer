a4aaed8d0eae177970783a4e2568c6dd798a004b
Shubhra Salunke <shubrasalunkecse@gmail.com>
2022-11-10 12:27:20 -0600

    Update index.js

--- src/server/index.js
+++ src/server/index.js
@@ -22,6 +22,7 @@ console.log(`Your API key is ${process.env.API_KEY}`);
 const app = express();
 
 app.use(cors());
+app.use(express.static("dist"));
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json());
