5c692aedd3fcbb355c47fcf2105a0d9e6e6bf6c0
Shubhra Salunke <shubrasalunkecse@gmail.com>
2022-11-09 20:36:57 -0600

    Update index.js

--- src/server/index.js
+++ src/server/index.js
@@ -26,7 +26,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json());
 
-app.use(express.static('dist'))
+app.use(express.static('./dist'))
 
 console.log(__dirname)
 
