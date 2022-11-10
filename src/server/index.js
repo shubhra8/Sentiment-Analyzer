8dac779f099d7f2d0fea569a0f52fd6e8c9030e3
Shubhra Salunke <shubrasalunkecse@gmail.com>
2022-11-10 10:17:18 -0600

    Update index.js

--- src/server/index.js
+++ src/server/index.js
@@ -48,8 +48,8 @@ const Data = await response.json()
 })
 
 app.get('/', function (req, res) {
-    //res.sendFile(path.join(__dirname, '../../dist/index.html'));
-   res.sendFile("index.html", { root: '../../dist' })
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
+   //res.sendFile("index.html", { root: '../../dist' })
     //res.sendFile("./index.html");
     //res.sendFile('index.html', { root: '/dist' })
     //res.sendFile('evaluate-news-nlp/dist/index.html');
