f8df4c20d6f0b241b8a114d91ae48cfded795243
Shubhra Salunke <shubrasalunkecse@gmail.com>
2022-11-10 12:09:35 -0600

    Update index.js

--- src/server/index.js
+++ src/server/index.js
@@ -49,7 +49,7 @@ const Data = await response.json()
 
 app.get('/', function (req, res) {
    // app.use(express.static(path.join(__dirname, 'dist/index.html')));
-   res.sendFile("index.html", { root: '/dist' })
+   res.sendFile("index.html", { root: '/app/dist' })
     //res.sendFile("./index.html");
     //res.sendFile('index.html', { root: '/dist' })
     //res.sendFile('evaluate-news-nlp/dist/index.html');
